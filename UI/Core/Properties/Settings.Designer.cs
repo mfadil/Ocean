1cbcac4878d6e5b1fa4481d4be41c5125a59f9d3
mfadil <mohammed.fadil@gmail.com>
2015-12-01 09:41:09 +0300

    Added Core project and setup references.

--- UI/Core/Properties/Settings.Designer.cs
+++ UI/Core/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Ocean.Presentation.Properties {
+namespace Ocean.UI.Core.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
